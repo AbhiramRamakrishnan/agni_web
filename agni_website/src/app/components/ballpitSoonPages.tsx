8e030fa281afa753ca7dc9fb7382b94626f78f48
Abhiram Ramakrishnan <111425672+AbhiramRamakrishnan@users.noreply.github.com>
2025-07-05 00:04:45 +0530

    Update ballpitSoonPages.tsx

--- agni_website/src/app/components/ballpitSoonPages.tsx
+++ agni_website/src/app/components/ballpitSoonPages.tsx
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+
 'use client';
 
 import React, { useRef, useEffect } from "react";
