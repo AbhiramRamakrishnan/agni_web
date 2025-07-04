ff29adb53c7e57993ceb8caa485d770ab072982b
Abhiram Ramakrishnan <111425672+AbhiramRamakrishnan@users.noreply.github.com>
2025-07-04 23:00:40 +0530

    Update page.tsx

--- agni_website/src/app/page.tsx
+++ agni_website/src/app/page.tsx
@@ -4,7 +4,6 @@ import Head from "next/head";
 import Image from "next/image";
 import Navbar from "./components/navbar";
 import Hero from "./components/hero";
-import ScrollVelocity from './components/scrollVelocity';
 import WhyThisMatters from "./components/Problem_statement";
 import Failures from "./components/failures";
 import Future from "./components/future";
@@ -12,8 +11,6 @@ import Solutions from "./components/Solution";
 import Why from "./components/why";
 import Benefits from "./components/benefits";
 import ContactForm from "./components/contactForm"
-import AgniLoader from "./components/loading";
-import HeroNew from "./components/hero_new"
 import dynamic from 'next/dynamic';
 
 
@@ -26,11 +23,6 @@ export default function Home() {
       </Head>
       <Navbar />
       <Hero />
-      {/* <ScrollVelocity
-        texts={['Agni Robotics ', 'Seed the change Rule the game']} 
-        velocity={100} 
-        className="custom-scroll-text pb-4 bg-white"
-      /> */}
       <WhyThisMatters />
       <Failures />
       <Future />  
@@ -39,14 +31,7 @@ export default function Home() {
       <Benefits />
       <ContactForm />
 
-      {/* <AgniLoader /> */}
-
-        {/* <HeroNew /> */}
-      {/* <VerticalCarousel cards={carouselData} autoRotateInterval={2000} /> */}
-
-      {/* <main className="min-h-screen bg-neutral-100 dark:bg-neutral-950">
-        <UpliftingCards />
-      </main> */}
+    
     </>
   );
 }
