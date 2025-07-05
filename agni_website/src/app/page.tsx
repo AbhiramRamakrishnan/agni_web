062b6adfedf1cf35033be08022cfc944b8b68d9b
Abhiram Ramakrishnan <111425672+AbhiramRamakrishnan@users.noreply.github.com>
2025-07-05 12:33:26 +0530

    Update page.tsx

--- agni_website/src/app/page.tsx
+++ agni_website/src/app/page.tsx
@@ -22,9 +22,6 @@ import dynamic from 'next/dynamic';
 export default function Home() {
   return (
     <>
-{/*       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head> */}
       <Navbar />
       <Hero />
       <WhyThisMatters />
